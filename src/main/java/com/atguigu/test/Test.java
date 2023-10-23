3b4b17930c2f263f73b8840b336d60e7ecc07473
王小瑞2 <13232495+wang-xiaorui-2@user.noreply.gitee.com>
2023-10-23 12:49:12 +0800

    idea push commit

--- src/main/java/com/atguigu/test/Test.java
+++ src/main/java/com/atguigu/test/Test.java
@@ -7,5 +7,6 @@ public class Test {
         System.out.println("hello2 by new branch");
         System.out.println("hello2 by master01 branch");
         System.out.println("hello2 by new branch02 ");
+        System.out.println("try push");
     }
 }
