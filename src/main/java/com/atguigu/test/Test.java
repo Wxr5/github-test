d4cbee1cd5bfbeb5793fc63ffda6aa627ab4a2ff
Wxr5 <106417752+Wxr5@users.noreply.github.com>
2023-10-23 13:00:22 +0800

    Update Test.java
    try yuancheng

--- src/main/java/com/atguigu/test/Test.java
+++ src/main/java/com/atguigu/test/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("hello2 by master01 branch");
         System.out.println("hello2 by new branch02 ");
         System.out.println("try push");
+        System.out.println("try remote pull native");
     }
 }
